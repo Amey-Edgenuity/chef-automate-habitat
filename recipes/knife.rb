05872ba5b93e670974a669e65bf9d86744d92871
Ramit Surana <ramitsurana@gmail.com>
2018-01-11 17:01:07 +0530

    add extra parameters

--- recipes/knife.rb
+++ recipes/knife.rb
@@ -1,2 +1,6 @@
 knife[:aws_access_key_id]     = "<ACCESS-ID>"
 knife[:aws_secret_access_key] = "<ACCESS-SECRET_ID>" 
+knife[:flavor] = 't2.micro'
+knife[:image] = 'ami-aa2ea6d0' # Ubuntu 16.04 
+knife[:aws_ssh_key_id] = '<SSH-KEY-ID>'
+knife[:identity_file] = "<YOUR-IDENTITY>.pem"
