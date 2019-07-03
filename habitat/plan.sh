e54086b2b63306239e83beb48470f6c0d3c5d854
Amey-Edgenuity <47824662+Amey-Edgenuity@users.noreply.github.com>
2019-07-03 13:15:29 +0530

    Add files via upload

---
+++ habitat/plan.sh
@@ -0,0 +1,18 @@
+pkg_origin=amey_test
+pkg_name=hadoop
+pkg_version="0.0.1"
+pkg_license=('MIT')
+pkg_maintainer="Ramit Surana <ramitsurana@gmail.com>"
+pkg_upstream_url=https://github.com/ramitsurana/chef-automate-habitat
+pkg_deps=(core/vim core/jre8)
+
+pkg_binds=(
+  return 0
+  )
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
