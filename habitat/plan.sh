0cf71fdcf4a83769dc57171510b60c2eeb00fd95
Amey-Edgenuity <47824662+Amey-Edgenuity@users.noreply.github.com>
2019-07-03 14:27:53 +0530

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,7 +2,7 @@ pkg_origin=amey_test
 pkg_name=hadoop
 pkg_version="0.0.1"
 pkg_license=('MIT')
-pkg_maintainer="Ramit Surana <ramitsurana@gmail.com>"
+pkg_maintainer="Amey K <ask712@gmail.com>"
 pkg_upstream_url=https://github.com/ramitsurana/chef-automate-habitat
 pkg_deps=(core/vim core/jre8)
 
